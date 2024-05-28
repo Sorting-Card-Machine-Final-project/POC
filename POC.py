195451cc13b7aed779c2f27998460a415097ee2d
Yuval <yuval.halperin7@gmail.com>
2024-05-28 17:55:03 +0300

    Reorganize

---
+++ POC.py
@@ -0,0 +1,21 @@
+NUM_OF_CARDS = 52
+suits = {"Hearts":1,
+         "Diamonds" : 2,
+         "Clubs" : 3,
+         "Spades" : 4}
+class card():
+    def __init__(self) -> None:
+        self.number
+        self.suit
+
+
+class Pack():
+    def __init__(self) -> None:
+        self.cards
+        self.nums_of_sorts = 0
+        
+
+print(suits["Hearts"])
+print(suits["Diamonds"])
+print(suits["Clubs"])
+# Arrange the for loops to run on a pair of the dic and add the numbers
